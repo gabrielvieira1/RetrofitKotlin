96eb313825bd0590faf94ca2346a151a5eb3610f
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-03-16 18:17:44 -0300

    Learning about Retrofit in Kotlin

---
+++ app/src/main/java/com/android/retrofitkotlin/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.android.retrofitkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private val remote = RetrofitClient.createService(PostService::class.java)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val call: Call<List<PostModel>> = remote.list()
+        call.enqueue(object : Callback<List<PostModel>> {
+            override fun onFailure(call: Call<List<PostModel>>, t: Throwable) {
+                val fail = t.message
+            }
+
+            override fun onResponse(call: Call<List<PostModel>>, rsp: Response<List<PostModel>>) {
+                val list = rsp.body()
+            }
+        })
+    }
+}
